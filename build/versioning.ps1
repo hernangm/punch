edfa8b704465d0dec09ed0b9f2e664d2131cf37f
Hernan Marano <herchugm@gmail.com>
2014-07-30 22:02:22 -0300

    change of build scripts

--- build/versioning.ps1
+++ build/versioning.ps1
@@ -1,255 +1,158 @@
-﻿<#
-function New-Prueba([string]$initialValue)
- {
-    New-Module -AsCustomObject -name Prueba -ArgumentList $initialValue -scriptblock {
-        param([string]$initialValue)
-        $nombre = $null
-        $pepe = "Pepe"
-
-        function Init()
-        {
-            $nombre = $initialValue
-        }
-
-        Init
-        Export-ModuleMember -Variable *
-    } 
-}
-
-$prueba1 = New-Prueba "pepe"
-$prueba1.dato = "pepe"
-
-$prueba2 = New-Prueba
-$prueba2.dato = "raul"
-
-$prueba1.dato
-$prueba2.dato
-
-$ver = New-Version "1.0.0.1"
-
-$ver.OriginalValue()
-#>
-function New-AssemblyInfo([string]$path)
-{
-    New-Module -AsCustomObject -name AssemblyInfo -ArgumentList $path -scriptblock {
-        param([string]$path)
-        $versions= @{
-            Version = $null
-            FileVersion = $null
-            InformationalVersion = $null
-        }
-
-        function New-Version([string]$initialValue)
-        {
-    New-Module -AsCustomObject -name Version -ArgumentList $initialValue -scriptblock {
-        param([string]$initialValue)
-        $original = @{
-            Major = $null
-            Minor = $null
-            Build = $null
-            Revision = $null
-        }
-        $changed = @{
-            Major = $null
-            Minor = $null
-            Build = $null
-            Revision = $null
-        }
-
-        function OriginalValue()
-        {
-            return ConvertToString $original
-        }
-
-        function ChangedValue()
-        {
-            return ConvertToString $changed
-        }
-
-        function HasChanged()
-        {
-            $o = OriginalValue
-            $c = ChangedValue
-            return $o -ne $c
-        }
-
-        function Increment([string] $portion)
-        {
-            if (!$changed.ContainsKey($portion)) {
-                throw "The argument `$portion ($portion) is not valid."
-            }
-            $report = @{}
-            $report.OldValue = ChangedValue
-            $changed[$portion] +=1
-            $report.NewValue = ChangedValue
-            return $report
-        }
-
-        function Merge($version)
-        {
-            $report = @{}
-            $report.OldValue = ChangedValue
-            foreach($key in $version.changed.Keys)
-            {
-                $portionSource = $version.changed[$key]
-                if(($changed[$key] -ne $portionSource) -and ($portionSource -ne "*")){
-                    $changed[$key] = $portionSource
-                }
-            }
-            $report.NewValue = ChangedValue
-            return $report
-        }
-
-        function Reset()
-        {
-            $changed.Major = $null
-            $changed.Minor = $null
-            $changed.Build = $null
-            $changed.Revision = $null
-        }
-
-        function ConvertToString($source)
-        {
-            $arr = @()
-            $arr += [string]$source.Major
-            $arr += [string]$source.Minor
-            if(([string]($source.Build) -eq "*") -or ($source.Build -ge 0)){
-                $arr += [string]$source.Build
-            }
-            if(([string]($source.Revision) -eq "*") -or ($source.Revision -ge 0)){
-                $arr += [string]$source.Revision
-            }
-            $str = $($arr -join ".")
-            return $str
-        }
-
-        #========================================
-        # Private functions
-        #========================================
-        function Init()
-        {
-            $parsed = [Version]::Parse($initialValue.Replace("*",0))
-            $original.Major = FixValue $initialValue 0 $parsed.Major
-            $original.Minor = FixValue $initialValue 1 $parsed.Minor
-            $original.Build = FixValue $initialValue 2 $parsed.Build
-            $original.Revision = FixValue $initialValue 3 $parsed.Revision            
-
-            $changed.Major = $original.Major
-            $changed.Minor = $original.Minor
-            $changed.Build = $original.Build
-            $changed.Revision = $original.Revision
-        }
-
-        function FixValue([string]$_version,[int]$index,[int]$parsedValue)
-        {
-            if ($parsedValue -eq -1) {
-                return $parsedValue
-            }
-            $value = $_version.Split(".")[$index]
-            if ($value -eq "*") {
-                return $value
-            }
-            return [int]$value
-        }
-        
-		Init
-        Export-ModuleMember -Variable * -function OriginalValue, ChangedValue, HasChanged, Increment, Merge, Reset
-    } 
-        }
+﻿#========================================
+# Version Helper functions
+#========================================
+$delim = "."
 
 
-        function GetDefiniteVersion()
-        {
-            $version = $versions["InformationalVersion"]
-            if ($version -eq $null) { 
-                $version = $versions["Version"]
-            }
-            return $version.changedValue()
-        }
 
-        function IncrementVersions([string] $portion)
-        {
-            $output = @()
-            $output += $(IncrementVersion $portion)
-            $version = $versions["Version"]
-            foreach($key in $versions.Keys) {
-                if ($key -ne "Version") {
-                    $item = $versions[$key]
-                    if ($item -ne $null) {
-                        $report = $item.Merge($version)
-                        $report.VersionType = $key
-                        $output += $report
-                    }
-                }
-            }
-            return $output
-        }
-
-        function IncrementVersion([string] $portion) {
-            $key = "Version"
-            $version = $versions[$key]
-            $report = $version.Increment($portion)
-            $report.VersionType = $key
-            return $report
-        }
-
-        function RevertChanges()
-        {
-            foreach($key in $versions.Keys) {
-                $item = $versions[$key]
-                if ($item -ne $null) {
-                    $item.Revert()
-                }
-            }
-        }
-
-        function Save()
-        {
-            $contents = [System.IO.File]::ReadAllText($path)
-            $replaced = $false
-            foreach($key in $versions.Keys) {
-                $item = $versions[$key]
-                if ($item -ne $null -and $item.hasChanged()) {
-                    $originalValue = $item.OriginalValue()
-                    $changedValue = $item.ChangedValue()
-                    $search = GetReplacement $key $originalValue
-                    $replace = GetReplacement $key $changedValue
-                    $contents = $contents.Replace($search,$replace) 
-                    $replaced = $true
-                }
-            }
-            if ($replaced) {
-                [System.IO.File]::WriteAllText($path, $contents)
-            }
-        }
-
-        #========================================
-        # Private functions
-        #========================================
-        function Init()
-        {
-            $contents = [System.IO.File]::ReadAllText($path)
-            $versionNumberPattern = "(?<version>[0-9]+(?:\.(?:[0-9]+|\*)){1,3})"
-            foreach($key in @($versions.Keys)) {
-                $pattern = GetPattern $key $versionNumberPattern
-                $match = [Regex]::Match($contents,$pattern)
-                if ($match.Success) {
-                    $versions[$key] = New-Version $match.Groups["version"].Value
-                }
-            }
-        }
-
-        function GetPattern ([string]$versionType, [string]$value)
-        {
-            return [string]::Format("Assembly{0}\(`"{1}`"\)",$versionType, $value)
-        }
-
-        function GetReplacement ([string]$versionType, [string]$value)
-        {
-            return [string]::Format("Assembly{0}(`"{1}`")",$versionType, $value)
-        }
-        
-        Init
-        Export-ModuleMember -Variable * -function IncrementVersions, RevertChanges, GetDefiniteVersion, Save #-Variable *
+function Get-Dll-Version($dll_file) {
+	$resolved = Resolve-Path $dll_file
+	Write-Progress "Obtaining version from $dll_file"
+    Write-Debug " $resolved "
+    if(!(Test-Path $resolved)) {
+        throw "Cannot retrive assembly version because file '$resolved' does not exists."
     }
+    $version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($resolved).ProductVersion
+    #$version = [System.Reflection.Assembly]::LoadFrom($resolved).GetName().Version.ToString()
+    Write-Host $version
+    return $version
 }
+
+
+
+
+function Get-Incremented-Version-Numbers($version,$portion) {
+    $version_array =  [string[]] $version.Split('.')
+    $version_length = $version_array.Length
+	Write-Progress "Updating from $version to"
+    switch ($portion) 
+    { 
+        "major" {
+            if ($version_length -ge 1) {
+                #$version_array[0] += 1
+                $version_array[0] = Increment-Version $version_array[0]
+            } else {
+                throw
+            }
+        } 
+        "minor" {
+            if ($version_length -ge 2) {
+                #$version_array[1] += 1
+                $version_array[1] = Increment-Version $version_array[1]
+            } else {
+                throw
+            }
+        } 
+        "build" {
+            if ($version_length -ge 3) {
+                #$version_array[2] += 1
+                $version_array[2] = Increment-Version $version_array[2]
+            } else {
+                throw
+            }
+        } 
+        "revision" {            
+            if ($version_length -ge 3) {
+                #$version_array[3] += 1
+                $version_array[3] = Increment-Version $version_array[3]
+            } else {
+                throw
+            }
+        } 
+        default { throw "The argument `$portion ($portion) is not valid."}
+    }
+    $new_version = $version_array -join "."
+    Write-Success "$new_version`n"
+    return $new_version
+}
+
+function Increment-Version($portion){
+    $version_array =  ([string]$portion).Split('-')
+    $version_array[0] = ($version_array[0] -as [int]) + 1
+    return $version_array -join "-"
+}
+
+
+function Set-Assembly-Version([string[]]$assembly_files, [string] $version) {
+    if ($assembly_files.Count -eq 0) {
+        return
+    }
+    Write-Progress "Updating:`n"
+    $assembly_version_types = @(@{Label = "Version"; RemoveStrings = $true}, @{Label = "InformationalVersion"; RemoveStrings = $false},  @{Label = "FileVersion"; RemoveStrings = $true})
+    $assemblyPattern = "(?<version>[0-9]+(\.([0-9]+(\-[a-zA-Z0-9]*)?|\*)){1,3})"
+    foreach($file in $assembly_files) {
+
+        Write-Host "`n`n      $file`n"
+
+        $replaced = $false
+        [string] $contents = [System.IO.File]::ReadAllText($file)
+        foreach($version_type in $assembly_version_types) {
+            #AssemblyVersion\("([0-9]+(\.([0-9]+(\-[a-zA-Z0-9]*)?|\*)){1,3})"\)
+            $pattern = [string]::Format("Assembly{0}\(`"{1}`"\)",$version_type.Label,$assemblyPattern)
+            $match = [Regex]::Match($contents,$pattern)
+            if ($match.Success) {
+                [string] $old = $match.Groups["version"]
+                $new_version = Copy-Version $version $old $version_type.RemoveStrings
+                if($new_version -eq $old) {
+                    Write-Debug "$version_type is the same"
+                } else {
+                    $replacement = [string]::Format("Assembly{0}(`"{1}`")",$version_type.Label, $new_version)
+                    $contents = [Regex]::Replace($contents,$pattern,$replacement)
+                    $replaced = $true
+                    Write-Version-Update $version_type.Label $old $new_version
+            }
+        }
+        }
+        if ($replaced) {
+            [System.IO.File]::WriteAllText($file, $contents)
+            Write-Debug "Saved!"
+        }
+
+        }
+    return
+}
+        
+
+
+
+function Split-Version([string] $version) {
+    return $version.split($delim)
+}
+
+
+function Join-Version([string[]] $array) {
+    return $array -join $delim
+}
+
+
+function Copy-Version([string] $version1, [string] $version2, [boolean] $removeStrings) {
+    $copied = $false
+    $source_array = Split-Version $version1
+    $destination_array = Split-Version $version2
+    $upperLimit = [Math]::Min($source_array.Length, $destination_array.Length) - 1
+    for($i=0;$i -le $upperLimit; $i++) {
+        if($destination_array[$i] -ne "*" -and $destination_array[$i] -ne $source_array[$i]) {
+            $this = ($source_array[$i] -as [string])
+            $term = [string[]]$this.Split("-")
+            if ($term.Length -gt 1 -and $removeStrings -eq $true) {
+                $destination_array[$i] = $term[0]
+            } else {
+            $destination_array[$i] = $source_array[$i]
+                }
+            $copied = $true
+            }
+        }
+    if ($copied) {
+        return Join-Version $destination_array
+        }
+    return $version1
+}
+
+        
+function Write-Version-Update([string] $version_type, [string] $old_version, [string] $new_version) {
+    Write-Host "      Updated $version_type from $old_version to" -NoNewline
+    Write-Success $($new_version)
+}
+#========================================
+# /Version Helper functions
+#========================================
--- source/Punch.Tests/Properties/AssemblyInfo.cs
+++ source/Punch.Tests/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("b6ae8283-6119-4565-bfac-685d54147dfe")]
 [assembly: AssemblyTitle("Punch.Tests")]
 [assembly: AssemblyDescription("Punch.Tests")]
-[assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.54")]
+[assembly: AssemblyFileVersion("2.0.54")]
+[assembly: AssemblyInformationalVersion("2.0.54")]
 
 
